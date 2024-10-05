21fa8d0f5b7891dd9b583f6dc7da78f5da6d6f87
chunyukuo88 <alexgochenour@gmail.com>
2024-10-05 14:49:45 -0400

    feat(useAuth.ts): unhappy path test & implementation.

--- __tests__/hooks/useAuth.test.tsx
+++ __tests__/hooks/useAuth.test.tsx
@@ -8,26 +8,48 @@ jest.mock("aws-amplify/auth", () => ({
   signOut: jest.fn(),
 }));
 
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("useAuth.ts", () => {
   describe("useAuth()", () => {
     describe("WHEN: the hook is executed,", () => {
-      test("THEN: it returns a boolean indicating as much ", async () => {
-        const mockUser = {
-          username: "username",
-          userId: "userId",
-          signInDetails: {
-            loginId: "loginId",
-            authFlowType: "USER_PASSWORD_AUTH",
-          },
-        };
-        (signIn as jest.Mock).mockImplementationOnce(() => {});
-        (getCurrentUser as jest.Mock).mockResolvedValueOnce(mockUser);
+      describe("AND: the signInUser() HOC is executed with valid username and password", () => {
+        test("THEN: it returns a boolean indicating as much ", async () => {
+          const mockUser = {
+            username: "username",
+            userId: "userId",
+            signInDetails: {
+              loginId: "loginId",
+              authFlowType: "USER_PASSWORD_AUTH",
+            },
+          };
+          (signIn as jest.Mock).mockImplementationOnce(() => {});
+          (getCurrentUser as jest.Mock).mockResolvedValueOnce(mockUser);
 
-        const { signInUser } = useAuth();
+          const { signInUser } = useAuth();
 
-        await waitFor(async () => {
-          const user = await signInUser('username', 'password');
-          expect(user).toBe(mockUser);
+          await waitFor(async () => {
+            const user = await signInUser('username', 'password');
+            expect(user).toBe(mockUser);
+          });
+        });
+      });
+      describe("AND: the signInUser() HOC is executed with invalid username and password", () => {
+        test("THEN: it logs an error.", async () => {
+          const error = new Error('failed to get user');
+          const spy = jest.spyOn(console, "error").mockImplementationOnce(() => {});
+          (signIn as jest.Mock).mockImplementationOnce(() => {});
+          (getCurrentUser as jest.Mock).mockRejectedValue(error);
+
+          const { signInUser } = useAuth();
+
+          await waitFor(async () => {
+            await signInUser('invalid username', 'wrong password');
+            expect(spy).toBeCalledTimes(1);
+            expect(spy).toBeCalledWith(error);
+          });
         });
       });
     });
--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -10,14 +10,18 @@ interface UseAuthResult {
 }
 
 export function useAuth() {
-  const signInUser = async (username: string, password: string):Promise<AuthUser> => {
-    await signIn({
-      username,
-      password,
-    });
-    return await getCurrentUser();
-  }
+    const signInUser = async (username: string, password: string): Promise<AuthUser> => {
+      try {
+        await signIn({
+          username,
+          password,
+        });
+        return await getCurrentUser();
+      } catch (e) {
+        console.error(e);
+      }
+    }
 
-  return { signInUser };
+    return {signInUser};
 }
 
