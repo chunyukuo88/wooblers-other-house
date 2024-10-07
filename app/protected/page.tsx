a5c69f79ed2e77cd138fb68350444aee5ee80c5b
chunyukuo88 <alexgochenour@gmail.com>
2024-10-06 22:25:19 -0400

    refactor(images.tsx): extraction, quotation marks

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,14 +1,12 @@
-import {getServerSession} from "next-auth";
-import {redirect} from "next/navigation";
-import {allPaths} from "../../allPaths";
+// import {getServerSession} from "next-auth";
+// import {redirect} from "next/navigation";
+// import {allPaths} from "../../allPaths";
 
 export default async function ProtectedRoute() {
-  const session = await getServerSession();
-  console.log("session:");
-  console.log(session);
-  if (!session || !session.user) {
-    redirect(allPaths.LOGIN);
-  }
+  // const session = await getServerSession();
+  // if (!session || !session.user) {
+  //   redirect(allPaths.LOGIN);
+  // }
 
   return (
     <div>
