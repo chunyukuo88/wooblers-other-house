cb1dca3ba1c8953a09352c9a38ec575da79b4e6d
chunyukuo88 <alexgochenour@gmail.com>
2024-10-14 23:27:49 -0400

    feat: wiggly cars for woobler

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import ImageGallery from "@/components/image-gallery/image-gallery";
 
 export const metadata = {
-  title: process.env.NODE_ENV === "production" ? "Vrooooooom!" : "小巫之另一個屋",
+  title: process.env.NODE_ENV === "production" ? "🚗 Vrooooooom!" : "小巫之另一個屋",
 };
 
 export default function Page() {
--- components/navigation/site-title-string.tsx
+++ components/navigation/site-title-string.tsx
@@ -1,12 +1,15 @@
 import {allPaths} from "../../allPaths";
 import Link from "next/link";
+import "./site-title-string.css";
 
 export default function SiteTitleString() {
   return (
-    <h1>
-      <Link href={allPaths.HOME}>
-        Woobler's Other House
-      </Link>
+    <h1 className="woh__site-title-string">
+      <div className="woh__wiggling-cars">🚛 </div>
+        <Link href={allPaths.HOME}>
+          Woobler's Other House
+        </Link>
+      <div className="woh__wiggling-cars"> 🚔</div>
     </h1>
   )
 }
