40d07c24b2a0b31d9cd71cbb3d86a6acb80817c7
chunyukuo88 <alexgochenour@gmail.com>
2024-10-16 23:13:47 -0400

    chore: deleted comments

--- __tests__/components/image-gallery/image-card.test.tsx
+++
@@ -1,9 +0,0 @@
-// import {getCaptions} from "@/components/image-gallery/image-card";
-//
-// describe('GIVEN: a captions file,', () => {
-//   describe('WHEN: the file contains captions,', () => {
-//     test('THEN: it returns an array of captions,', () => {
-//       const captionsFile = {}
-//     });
-//   });
-// });
