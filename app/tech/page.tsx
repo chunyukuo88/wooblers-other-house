86fb2a894532e17754bc46ee7117fd12bda925c4
chunyukuo88 <alexgochenour@gmail.com>
2024-10-24 16:49:21 -0400

    feat: page and some content
    - Also created route and nav button

---
+++ app/tech/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+import {useEffect, useState} from "react";
+import Image from "next/image";
+
+export default function Page() {
+
+  return (
+    <main id="tech-page">
+      <h1>How this web app was made</h1>
+      <h2>A dev diary showcasing the technologies that went into Woobler's Other House</h2>
+      <p>
+        I created this Next.js app as a sort of sandbox to try new things. Working for Gap Inc., I led the Product
+        Listing Page team's migration from being one of a constellation of React SPAs to being a part within a massive
+        Next.js monorepo.
+      </p>
+      <p>
+        There were a few technologies I'd not played with much in my own applications, such as
+        the <code>IntersectionObserver</code> API or NextAuth.
+        This app would be a meaningful place to maintain them and see how they evolve along with Next.js.
+      </p>
+      <div className="woh__tech-page__next-logo">
+        <Image
+          alt="Next.js logo"
+          src="/images/woobler-pointing.png"
+          width={200}
+          height={200}
+        />
+      </div>
+      <p>In past jobs I used React Router, Tanstack Router, and SvelteKit routing. The latter felt the most like the
+        app-based routing of Next.js, at least in version 14. I then learned that Vercel manages both technologies. They
+        hired Rich Harris in 2023!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet aperiam consectetur hic itaque
+        necessitatibus nihil! Alias at consectetur cumque eos in laborum neque nulla, officiis quam sit tempora veniam
+        veritatis voluptate. Accusamus dicta dolore dolorum expedita natus non odio provident sed voluptas. Blanditiis
+        cum dolor earum eos facere, ipsum libero natus provident quasi, qui saepe sequi similique tempora ut velit? Ad
+        animi error fuga hic in, inventore ipsam itaque nam repellendus tempora vel velit. Eaque ex excepturi, facilis
+        hic laudantium nostrum nulla odit rerum ut! Aperiam atque culpa cumque debitis deleniti dicta, dignissimos
+        dolorem doloremque doloribus eligendi enim eos error facere fugiat harum id ipsum laboriosam laborum maiores
+        minus, nisi officiis omnis optio, perferendis perspiciatis possimus quae qui quia quibusdam quod repellendus
+        totam veritatis voluptates! Aliquam, asperiores at atque blanditiis debitis, dignissimos earum eligendi facilis
+        illo minus molestiae necessitatibus numquam optio perspiciatis placeat provident sed! Animi asperiores
+        blanditiis commodi cumque debitis dignissimos facilis id, illum laborum magni quisquam recusandae? Fuga, numquam
+        perferendis. Aperiam assumenda, consectetur ducimus excepturi id numquam officia optio repellendus sapiente
+        voluptate. Alias architecto, debitis dolorem eum, hic omnis sapiente sint tempora tempore, unde ut veritatis!
+        Asperiores autem corporis, nisi odit quae qui sequi. Amet, ex molestiae non nulla numquam possimus quasi qui
+        rerum sunt voluptatum. Maxime minima natus unde. Assumenda dolorem est eveniet fugiat iure magnam nihil sed
+        voluptatum. Ab ad architecto aspernatur consequuntur corporis cumque dicta dolor eligendi enim eos facere fuga
+        harum id illo, incidunt ipsum libero magnam modi, nam nesciunt nulla, odio officiis omnis perspiciatis possimus
+        mollitia nisi numquam optio reprehenderit voluptatem. Excepturi, fugiat iste laudantium magnam odio porro quam
+        quos sapiente. Ab consequuntur cupiditate dolores eveniet facere harum id labore, magnam modi molestias mollitia
+        nisi, possimus quam quis repudiandae sequi tempore unde ut velit vitae. Dolore enim facilis magni nam
+        necessitatibus pariatur sed. Adipisci assumenda aut cumque eius eum fugiat magni, natus, nisi porro provident
+        quisquam saepe suscipit tempore, tenetur ullam? Delectus dolor doloremque doloribus est impedit itaque, labore
+        pariatur quaerat quidem sed. A accusamus alias, aliquam animi cumque deserunt facilis fugiat hic, inventore ipsa
+        non pariatur provident vitae! Autem commodi doloremque dolores eius eveniet perspiciatis quo sint temporibus
+        veritatis voluptate. Accusamus adipisci enim impedit ipsam laborum magni necessitatibus nulla numquam placeat
+        quasi recusandae reiciendis repellat, reprehenderit sunt ullam? Corporis dolor eligendi esse obcaecati tenetur?
+        A aliquid aut debitis delectus distinctio error ex explicabo, facere harum illum impedit, inventore magni maxime
+        officia, quae quam saepe sunt vero vitae voluptatem. Adipisci amet animi assumenda delectus deleniti dolore
+        earum error ex, in libero maxime minima minus natus necessitatibus obcaecati pariatur provident quasi ratione
+        repellat repellendus reprehenderit tempora, tempore tenetur ullam vero voluptas voluptatum. Atque, ipsum
+        molestias?</p>
+      <div className="woh__tech-page__vercel-logo">
+        <Image
+          alt="Vercel logo"
+          src="/images/woobler-pointing.png"
+          width={200}
+          height={200}
+        />
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquid amet autem commodi culpa, distinctio
+        est facilis hic in molestiae mollitia nemo nihil nobis nulla, odio officia porro possimus quae repellat
+        reprehenderit soluta ut voluptate! Aspernatur assumenda at atque aut consequatur culpa deserunt dolorem enim est
+        ex fugit ipsam iure libero magnam, molestiae nemo nostrum porro, quia quidem ratione rem reprehenderit sed sequi
+        sint tempore voluptates voluptatibus. A, aliquam, aliquid beatae cum cupiditate delectus dignissimos distinctio
+        doloremque ducimus error eum excepturi explicabo harum hic labore minus molestias nam natus neque non odit qui
+        quisquam rem, sed vel! Deleniti ducimus, eum harum labore molestias neque numquam soluta voluptatum? Amet,
+        consectetur consequuntur dolorem dolores harum illo in, laudantium minus numquam repudiandae sapiente ut! Ab
+        adipisci exercitationem iste laudantium minus perspiciatis tempore tenetur veritatis. Aspernatur cumque
+        distinctio eligendi fuga, impedit iure laboriosam maiores minus modi optio perferendis perspiciatis placeat quas
+        neque nesciunt numquam obcaecati odit pariatur placeat quibusdam reiciendis rem repellendus sit soluta tempore
+        unde veniam. Amet asperiores at consectetur cum cupiditate dolore, dolorum eligendi error est expedita hic in
+        ipsam iure labore libero mollitia nam neque nisi numquam odio officiis pariatur perferendis praesentium
+        provident quasi quia quo quos ratione rem reprehenderit sequi sint, tempora totam velit vitae voluptatem
+        voluptatum? Iste minima obcaecati optio reprehenderit. A adipisci atque enim eveniet expedita modi odit officiis
+        quibusdam repellendus vel! Iure, possimus, saepe! Accusamus accusantium at autem, commodi, dolorem eius esse
+        inventore iste iusto laborum laudantium magnam neque nihil placeat praesentium quas quasi quod ratione rem,
+        repellat reprehenderit sequi sit sunt vel vero? Impedit libero molestiae quaerat temporibus tenetur veritatis
+        voluptatem! Ab quaerat rerum voluptatem voluptatum!</p>
+      <div className="woh__tech-page__aws-logo">
+        <Image
+          alt="AWS logo"
+          src="/images/woobler-pointing.png"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="woh__tech-page__amplify-logo">
+        <Image
+          alt="Amplify logo"
+          src="/images/woobler-pointing.png"
+          width={200}
+          height={200}
+        />
+      </div>
+    </main>
+  );
+}
