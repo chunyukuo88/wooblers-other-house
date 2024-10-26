1de87fda6be67e7d24a72ede927c3942c45d91b2
chunyukuo88 <alexgochenour@gmail.com>
2024-10-25 23:09:47 -0400

    refactor: wrapped images extracted

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -3,6 +3,37 @@ import {useEffect, useState} from "react";
 import Image from "next/image";
 import "./tech.css";
 
+type FadeInImageProps = {
+  alt: string;
+  fadeInFromThe: "left" | "right";
+  height: number;
+  isVisible: boolean;
+  src: string;
+  width: number;
+}
+
+const FadeInImage = ({
+   alt,
+   fadeInFromThe,
+   height,
+   isVisible,
+   src,
+   width,
+ }: FadeInImageProps) => {
+  return (
+    <span className="woh__fade-in-container-left">
+      <div className={`woh__tech-page-image ${isVisible ? "fade-in" : ""}`}>
+        <Image
+          alt="Amplify logo"
+          src="/images/woobler-pointing.png"
+          width={100}
+          height={100}
+        />
+      </div>
+    </span>
+  );
+};
+
 export default function Page() {
   // Starts with image2 -- the first image is "above the fold".
   const [image2Visible, setImage2] = useState<boolean | null>(false);
@@ -35,6 +66,7 @@ export default function Page() {
     return () => {
       img2Observer.disconnect();
       img3Observer.disconnect();
+      img4Observer.disconnect();
     };
   }, []);
 
@@ -45,7 +77,7 @@ export default function Page() {
         I'm an engineer at Gap Inc., and Woobler is my younger son, to whom this site is dedicated. We like serifs.
       </h5>
       <h4>August 2024</h4>
-      <p>
+      <p className="woh__has-drop-letter">
         I am leading my team's apps from being two of a constellation of React
         SPAs to just another set of packages within a massive Next.js monorepo.
       </p>
@@ -63,61 +95,56 @@ export default function Page() {
             />
           </div>
         </span>
-        for many brands: Gap, Old Navy, Banana Republic, and Athleta.
-        Then add additional complexity of our sites for other countries, our commitment to accessibility, and the myriad
-        subtle features required to make a pleasant e-commerce experience; this is one of the world's largest clothing
-        giants, after all!
+        for many brands: Gap, Old Navy, Banana Republic, and Athleta. Then add additional complexity of our sites for
+        other countries, our commitment to accessibility, and the myriad subtle features required to make a pleasant
+        e-commerce experience; this is one of the world's largest clothing giants, after all!
       </div>
-      <p>
+      <p className="woh__has-drop-letter">
         My team was left out of the foundational work. I wanted to know what setting up a proper, complex Next.js was
         all about, so here we are!
       </p>
-
-      <h5>September 2024</h5>
-      <p>
-        This month I'm hoping to learn about the <code>IntersectionObserver</code> API, aggressive preloading, and NextAuth. Questions of authentication and authorization are now a
-        bit more complicated than in SPAs! This is a meaningful place to maintain them and see how they evolve along with Next.js.
-      </p>
-      <div className={`woh__tech-page-image ${image2Visible ? "fade-in" : ""}`}>
-        <Image
-          alt="NextAuth logo"
-          src="/images/logo_NextAuth.png"
-          width={100}
-          height={100}
-        />
+      <h4>September 2024</h4>
+      <div className="woh__has-drop-letter">
+        This month I'm hoping to learn about the <code>IntersectionObserver</code> API, aggressive preloading, and
+        NextAuth.
+        <span className="woh__fade-in-container">
+          <div className={`woh__tech-page-image ${image2Visible ? "fade-in" : ""}`}>
+            <Image
+              alt="NextAuth logo"
+              src="/images/logo_NextAuth.png"
+              width={100}
+              height={100}
+            />
+          </div>
+        </span>
+        Questions of authentication and authorization are now a bit more complicated than in SPAs! This is a meaningful
+        place to maintain them and see how they evolve along
+        with Next.js.
       </div>
-      <p>
+      <p className="woh__has-drop-letter">
         In past jobs I used React Router, Tanstack Router, and SvelteKit routing. The latter felt the most like the
         app-based routing of Next.js, at least in version 14. I then learned that Vercel manages both technologies. They
         hired Rich Harris in 2021!
       </p>
-      <div className={`woh__tech-page-image ${image3Visible ? "fade-in" : ""}`}>
-        <Image
-          alt="Vercel logo"
-          src="/images/woobler-pointing.png"
-          width={100}
-          height={100}
-        />
+      <div className="woh__has-drop-letter">
+        Probably the trickiest bit so far was getting AWS Amplify + Cognito + NextAuth to work harmoniously together.
+        Amplify wants environment variables in the web console, NextAuth wants a <code>secret</code> attribute in the
+        auth configuration object,
+          <span className="woh__fade-in-container-left">
+            <div className={`woh__tech-page-image ${image3Visible ? "fade-in" : ""}`}>
+              <Image
+                alt="Amplify logo"
+                src="/images/woobler-pointing.png"
+                width={100}
+                height={100}
+              />
+            </div>
+          </span>
+        and Cognito has options for everything under the sun. The final solution, not mentioned in the Amplify
+        configuration, was to write the environment variables to an inaccessible production .env document at build time.
+        That's something I had to figure out on my own. Google, Stack Overflow and ChatGPT just regurgitated the
+        NextAuth documentation.
       </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquid amet autem commodi culpa, distinctio
-        est facilis hic in molestiae mollitia nemo nihil nobis nulla, odio officia porro possimus quae repellat
-        reprehenderit soluta ut voluptate! Aspernatur assumenda at atque aut consequatur culpa deserunt dolorem enim est
-        ex fugit ipsam iure libero magnam, molestiae nemo nostrum porro, quia quidem ratione rem reprehenderit sed sequi
-        sint tempore voluptates voluptatibus. A, aliquam, aliquid beatae cum cupiditate delectus dignissimos distinctio
-        doloremque ducimus error eum excepturi explicabo harum hic labore minus molestias nam natus neque non odit qui
-        quisquam rem, sed vel! Deleniti ducimus, eum harum labore molestias neque numquam soluta voluptatum? Amet,
-        consectetur consequuntur dolorem dolores harum illo in, laudantium minus numquam repudiandae sapiente ut! Ab
-        adipisci exercitationem iste laudantium minus perspiciatis tempore tenetur veritatis. Aspernatur cumque
-        distinctio eligendi fuga, impedit iure laboriosam maiores minus modi optio perferendis perspiciatis placeat quas
-        neque nesciunt numquam obcaecati odit pariatur placeat quibusdam reiciendis rem repellendus sit soluta tempore
-        unde veniam. Amet asperiores at consectetur cum cupiditate dolore, dolorum eligendi error est expedita hic in
-        ipsam iure labore libero mollitia nam neque nisi numquam odio officiis pariatur perferendis praesentium
-        provident quasi quia quo quos ratione rem reprehenderit sequi sint, tempora totam velit vitae voluptatem
-        voluptatum? Iste minima obcaecati optio reprehenderit. A adipisci atque enim eveniet expedita modi odit officiis
-        quibusdam repellendus vel! Iure, possimus, saepe! Accusamus accusantium at autem, commodi, dolorem eius esse
-        inventore iste iusto laborum laudantium magnam neque nihil placeat praesentium quas quasi quod ratione rem,
-        repellat reprehenderit sequi sit sunt vel vero? Impedit libero molestiae quaerat temporibus tenetur veritatis
-        voluptatem! Ab quaerat rerum voluptatem voluptatum!</p>
       <div className={`woh__tech-page-image ${image4Visible ? "fade-in" : ""}`}>
         <Image
           alt="AWS logo"
