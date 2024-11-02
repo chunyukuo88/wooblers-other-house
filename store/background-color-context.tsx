605f4d2c0c21ace39fd701a94fadfd73b8a8ede8
chunyukuo88 <alexgochenour@gmail.com>
2024-11-02 12:22:47 -0400

    fix: added conditionality and moved logic
    - checks for existence of window
    - these checks are now in useEffect
    - this resolves SSR problems that precluded build

--- store/background-color-context.tsx
+++ store/background-color-context.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {createContext, useState, type PropsWithChildren} from "react";
+import {createContext, useState, type PropsWithChildren, useEffect} from "react";
 
 enum COLORS {
   RED = "red",
@@ -7,46 +7,62 @@ enum COLORS {
   BLUE = "blue",
 }
 
-const getDefaultColor = (color: string): number => {
-  const storageResult = localStorage.getItem(color);
-  return (storageResult)
-    ? parseInt(storageResult, 10)
-    : 255;
-};
-
-const defaultColorInteger_R = getDefaultColor(COLORS.RED);
-const defaultColorInteger_G = getDefaultColor(COLORS.GREEN);
-const defaultColorInteger_B = getDefaultColor(COLORS.BLUE);
-
 export const BackgroundColorContext = createContext({
-  backgroundColor_R: defaultColorInteger_R,
-  backgroundColor_G: defaultColorInteger_G,
-  backgroundColor_B: defaultColorInteger_B,
-  updateBackgroundColor_R: function(colorInteger_R: number) {},
-  updateBackgroundColor_G: function(colorInteger_G: number) {},
-  updateBackgroundColor_B: function(colorInteger_B: number) {},
-  sumOfColors: (255 * 3),
+  backgroundColor_R: 255,
+  backgroundColor_G: 255,
+  backgroundColor_B: 255,
+  updateBackgroundColor_R: (colorInteger_R: number) => {},
+  updateBackgroundColor_G: (colorInteger_G: number) => {},
+  updateBackgroundColor_B: (colorInteger_B: number) => {},
+  sumOfColors: 765, // 255 * 3
 });
 
 export function CaptionColorProvider(props: PropsWithChildren) {
-  const [color_R, setColor_R] = useState(defaultColorInteger_R);
-  const [color_G, setColor_G] = useState(defaultColorInteger_G);
-  const [color_B, setColor_B] = useState(defaultColorInteger_B);
-  const [sum, setSum] = useState((color_R + color_G + color_B));
+  const [color_R, setColor_R] = useState(255);
+  const [color_G, setColor_G] = useState(255);
+  const [color_B, setColor_B] = useState(255);
+  const [sum, setSum] = useState(765);
+
+  const getDefaultColor = (color: string): number => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      return 255;
+    }
+    const storageResult = window.localStorage?.getItem(color);
+    return (storageResult)
+      ? parseInt(storageResult, 10)
+      : 255;
+  };
+
+  useEffect(() => {
+    const defaultColor_R = getDefaultColor(COLORS.RED);
+    const defaultColor_G = getDefaultColor(COLORS.GREEN);
+    const defaultColor_B = getDefaultColor(COLORS.BLUE);
+
+    setColor_R(defaultColor_R);
+    setColor_G(defaultColor_G);
+    setColor_B(defaultColor_B);
+    setSum(defaultColor_R + defaultColor_G + defaultColor_B);
+  }, []);
 
   function updateTheColor_R(newColor_R: number){
     setColor_R(newColor_R);
-    localStorage.setItem("red", newColor_R.toString());
+    if (typeof window !== 'undefined' && window.localStorage) {
+      window.localStorage.setItem("red", newColor_R.toString());
+    }
     setSum((color_R + color_G + color_B));
   }
   function updateTheColor_G(newColor_G: number){
     setColor_G(newColor_G);
-    localStorage.setItem("green", newColor_G.toString());
+    if (typeof window !== 'undefined' && window.localStorage) {
+      window.localStorage.setItem("green", newColor_G.toString());
+    }
     setSum((color_R + color_G + color_B));
   }
   function updateTheColor_B(newColor_B: number){
     setColor_B(newColor_B);
-    localStorage.setItem("blue", newColor_B.toString());
+    if (typeof window !== 'undefined' && window.localStorage) {
+      window.localStorage.setItem("blue", newColor_B.toString());
+    }
     setSum((color_R + color_G + color_B));
   }
 
