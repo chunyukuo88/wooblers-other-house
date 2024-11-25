df873c975f0aa7bf6f197330059b97a2900e9f14
chunyukuo88 <alexgochenour@gmail.com>
2024-11-24 20:07:35 -0500

    refactor: margins is now an array of strings

--- components/galleries/utils.ts
+++ components/galleries/utils.ts
@@ -1,16 +1,28 @@
-const rulesetValues = {
-  2: { rotations: [-15, 15], margins: [100, -100]},
-  3: { rotations: [-15, 1, 15], margins: [-200, 0, 200]},
-  4: { rotations: [-15, -5, 5, 15], margins: [-200, -100, 100, 200]},
-  5: {rotations: [-20, -10, 0, 10, 20],  margins: [-200, -100, 0, 100, 200]},
-  6: {rotations: [25, -13, -2, 2, 13, 25],  margins: [-200, -100, -50, 50, 100, 200]},
-  7: {rotations: [25, -13, -2, 0, 2, 13, 25],  margins: [-200, -100, -50, 0, 50, 100, 200]},
-};
+const rulesetValues = [
+  {rotations: [-15, 15], margins: ["0 100px 0 0", "0", "0 -100px 0 0"]},
+  {rotations: [-15, 1, 15], margins: ["0 -200px 0 0", 0, "0 200px 0 0"]},
+  {
+    rotations: [-15, -5, 5, 15],
+    margins: ["0 -200px 0 0", "0 -100px 0 0", "0 100px 0 0", "0 200px 0 0"]
+  },
+  {
+    rotations: [-20, -10, 0, 10, 20],
+    margins: ["0 -200px 0 0", "0 -100px 0 0", "0 0px 0 0", "0 100px 0 0", "0 200px 0 0"]
+  },
+  {
+    rotations: [25, -13, -2, 2, 13, 25],
+    margins: ["0 -200px 0 0", "0 -100px 0 0", "0 -50px 0 0", "0 50px 0 0", "0 100px 0 0", "0 200px 0 0"]
+  },
+  {
+    rotations: [25, -13, -2, 0, 2, 13, 25],
+    margins: ["0 -200px 0 0", "0 -100px 0 0", "0 -50px 0 0", "0 0px 0 0", "0 50px 0 0", "0 100px 0 0", "0 200px 0 0"]
+  }
+];
 
 export function calculateStyle (arrayOfUrls: string[], i: number) {
     return {
-      transform:  `rotate(${rulesetValues[arrayOfUrls.length].rotations[i - 1]}deg)`,
-      marginRight: `${rulesetValues[arrayOfUrls.length].margins[i - 1]}px`,
-      justifySelf: "center"
+      transform:  `rotate(${rulesetValues[arrayOfUrls.length - 2].rotations[i - 1]}deg)`,
+      margin: `${rulesetValues[arrayOfUrls.length - 2].margins[i - 1]}`,
+      justifySelf: "center",
     };
-};
+}
