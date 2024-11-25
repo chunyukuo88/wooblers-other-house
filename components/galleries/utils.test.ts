da9e42897c8d726aea27e66487fc20d3f520efa8
chunyukuo88 <alexgochenour@gmail.com>
2024-11-24 19:46:07 -0500

    feat: produced the formula for card styling.

--- components/galleries/utils.test.ts
+++ components/galleries/utils.test.ts
@@ -9,7 +9,7 @@ describe("calculateStyle/2", () => {
     `("WHEN: there are 2 urls in the array,", ({index, rotation, margin}) => {
       describe(`AND: index equals ${index},`, () => {
         it(`THEN: returns a CSS object for card number ${index}`, () => {
-          const arrayOfUrls = ["",""];
+          const arrayOfUrls = Array(2).fill("www.example.com/image.jpg");
 
           const expected = {
             transform:  `rotate(${rotation}deg)`,
@@ -31,7 +31,30 @@ describe("calculateStyle/2", () => {
     `("WHEN: there are 3 urls in the array,", ({index, rotation, margin}) => {
       describe(`AND: index equals ${index},`, () => {
         it(`THEN: returns a CSS object for card number ${index}`, () => {
-          const arrayOfUrls = ["","",""];
+          const arrayOfUrls = Array(3).fill("www.example.com/image.jpg");
+
+          const expected = {
+            transform:  `rotate(${rotation}deg)`,
+            marginRight: `${margin}px`,
+            justifySelf: "center"
+          };
+
+          const result = calculateStyle(arrayOfUrls, index);
+
+          expect(result).toEqual(expected);
+        });
+      });
+    });
+    describe.each`
+      index   | rotation | margin
+      ${1}    | ${-15}   | ${-200}
+      ${2}    | ${-5}    | ${-100}
+      ${3}    | ${5}     | ${100}
+      ${4}    | ${15}    | ${200}
+    `("WHEN: there are 4 urls in the array,", ({index, rotation, margin}) => {
+      describe(`AND: index equals ${index},`, () => {
+        it(`THEN: returns a CSS object for card number ${index}`, () => {
+          const arrayOfUrls = Array(4).fill("");
 
           const expected = {
             transform:  `rotate(${rotation}deg)`,
--- components/galleries/utils.ts
+++ components/galleries/utils.ts
@@ -1,43 +1,16 @@
 const rulesetValues = {
-  2: {
-    rotations: [-15, 15],
-    margins: [100, -100],
-  },
-  3: {
-    rotations: [-15, 1, 15],
-    margins: [-200, 0, 200],
-  }
+  2: { rotations: [-15, 15], margins: [100, -100]},
+  3: { rotations: [-15, 1, 15], margins: [-200, 0, 200]},
+  4: { rotations: [-15, -5, 5, 15], margins: [-200, -100, 100, 200]},
+  5: {rotations: [-20, -10, 0, 10, 20],  margins: [-200, -100, 0, 100, 200]},
+  6: {rotations: [25, -13, -2, 2, 13, 25],  margins: [-200, -100, -50, 50, 100, 200]},
+  7: {rotations: [25, -13, -2, 0, 2, 13, 25],  margins: [-200, -100, -50, 0, 50, 100, 200]},
 };
 
 export function calculateStyle (arrayOfUrls: string[], i: number) {
-  if (arrayOfUrls.length === 2) {
     return {
       transform:  `rotate(${rulesetValues[arrayOfUrls.length].rotations[i - 1]}deg)`,
       marginRight: `${rulesetValues[arrayOfUrls.length].margins[i - 1]}px`,
       justifySelf: "center"
     };
-  }
-  if (arrayOfUrls.length === 3) {
-    return {
-      transform:  `rotate(${rulesetValues[arrayOfUrls.length].rotations[i - 1]}deg)`,
-      marginRight: `${rulesetValues[arrayOfUrls.length].margins[i - 1]}px`,
-      justifySelf: "center"
-    };
-  }
-  if (arrayOfUrls.length === 4) {
-    const rotations = [-15, -5, 5, 15];
-    return { transform:  `rotate(${rotations[i]}deg)`, marginRight: "-200px", justifySelf: "center" };
-  }
-  if (arrayOfUrls.length === 5) {
-    const rotations = [-15, -5, 0, 5, 15];
-    return { transform:  `rotate(${rotations[i]}deg)`, marginRight: "-200px", justifySelf: "center" };
-  }
-  if (arrayOfUrls.length === 6) {
-    const rotations = [-20, -10, -5, 5, 10, 20];
-    return { transform:  `rotate(${rotations[i]}deg)`, marginRight: "-200px", justifySelf: "center" };
-  }
-  if (arrayOfUrls.length === 7) {
-    const rotations = [-20, -10, -5, 0, 5, 10, 20];
-    return { transform:  `rotate(${rotations[i]}deg)`, marginRight: "-200px", justifySelf: "center" };
-  }
 };
