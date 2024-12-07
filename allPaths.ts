6d393cf48105901bfc947dc1989f6d20d5a25496
chunyukuo88 <alexgochenour@gmail.com>
2024-12-06 20:23:56 -0500

    feat(allPaths): email route added

--- allPaths.ts
+++ allPaths.ts
@@ -1,11 +1,13 @@
 const DASHBOARD = "/dashboard";
 const PROFILE = "/profile";
+const EMAIL_API_ROUTE = "/api/send-email";
 
-const protectedPaths = [DASHBOARD, PROFILE];
+const protectedPaths = [DASHBOARD, PROFILE, EMAIL_API_ROUTE];
 
 const allPaths = {
   BREAD: "/bread",
   DASHBOARD,
+  EMAIL_API_ROUTE,
   HOME: "/",
   LOGIN: "/login",
   PROFILE,
