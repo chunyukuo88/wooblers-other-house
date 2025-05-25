31703e0112dedf24cb1318e93b5a5fcca85a4f52
chunyukuo88 <alexgochenour@gmail.com>
2025-05-25 14:01:15 -0400

    fix: providers.tsx function signature

--- store/providers.tsx
+++ store/providers.tsx
@@ -6,17 +6,16 @@ import {FetchedImagesProvider} from "./fetched-images-context";
 import {CaptionColorProvider} from "./background-color-context";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-interface Children {
+interface PageProps {
   children: ReactNode;
+  session: Session;
 }
-
-type PageProps = { session: Session | null | undefined; }
 
 const queryClient = new QueryClient();
 
-export default function Providers({children}: Children, pageProps: PageProps) {
+export default function Providers({children, session}: PageProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
         <CaptionColorProvider>
           <FetchedImagesProvider>
