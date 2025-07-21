d324ef6890eef8eb122e12e477fd5e522119bf88
chunyukuo88 <alexgochenour@gmail.com>
2025-07-21 19:48:48 -0400

    fix: guard clause

--- components/navigation/album-selector.tsx
+++ components/navigation/album-selector.tsx
@@ -1,11 +1,13 @@
-import {convertFolderNameToDate} from "@/components/navigation/utils";
+"use client";
+// import {convertFolderNameToDate} from "@/components/navigation/utils";
 import {ChangeEvent} from "react";
 import {useMainImages} from "../../store";
+import {Folder} from "../../store/types";
 import "./album-selector.css";
 
 export const AlbumSelector = (props: any) => {
   const { style } = props;
-  const {fetchedFolders, updateCurrentFolder} = useMainImages();
+  const {fetchedFolders, isSuccess, updateCurrentFolder} = useMainImages();
 
   const changeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
     const index  = event.target.selectedIndex - 1;
@@ -17,6 +19,26 @@ export const AlbumSelector = (props: any) => {
 
   return (
     <select name="album-picked" id="woh__album-picker" onChange={changeHandler} style={style}>
+        {!fetchedFolders || !isSuccess
+          ? <AlbumsLoading style={style}/>
+          : null
+        }
+        {fetchedFolders
+          ? <Albums fetchedFolders={fetchedFolders}/>
+          : null
+        }
+    </select>
+  );
+}
+
+type AlbumsProps = {
+  fetchedFolders: Folder[];
+}
+
+function Albums(props: AlbumsProps){
+  const {fetchedFolders} = props;
+  return (
+    <>
       <option value="">Pick album</option>
       {fetchedFolders.map((folder, index) => {
         return (
@@ -25,6 +47,14 @@ export const AlbumSelector = (props: any) => {
           </option>
         );
       })}
+    </>
+  );
+}
+
+function AlbumsLoading(style: any) {
+  return (
+    <select name="album-picked" id="woh__album-picker" style={style}>
+      <option value="">Loading albums...</option>
     </select>
   );
 }
