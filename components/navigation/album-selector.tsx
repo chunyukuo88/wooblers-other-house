7638224292e66458c247ef16fd2a78d5f1b33ae9
chunyukuo88 <alexgochenour@gmail.com>
2025-07-22 00:19:45 -0400

    feat: undoing bodge
    - The problem was in the API all along;
    - Specifically, I forgot to deploy to PROD.

--- components/navigation/album-selector.tsx
+++ components/navigation/album-selector.tsx
@@ -29,14 +29,12 @@ type AlbumsProps = {
 
 function Albums(props: AlbumsProps){
   const {fetchedFolders} = props;
-  console.log('fetchedFolders:');
-  console.dir(fetchedFolders)
   return (
     <>
       {fetchedFolders.map((folder, index) => {
         return (
           <option className="woh__album-picker__option" key={index} value={folder.friendlyName}>
-            {index}
+            {folder.friendlyName || index}
           </option>
         );
       })}
