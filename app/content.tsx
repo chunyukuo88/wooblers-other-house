ae6682ef8436dc322b8c44378e62b6136a9576be
chunyukuo88 <alexgochenour@gmail.com>
2025-12-30 01:15:10 -0500

    WIP
    - Snowflakes effect

--- app/content.tsx
+++ app/content.tsx
@@ -4,6 +4,7 @@ import SiteTitleString from "@/components/navigation/components/site-title-strin
 import NavBar from "@/components/navigation/components/nav-bar";
 import {BackgroundColorContext as context} from "../store/background-color/context";
 import {calculateFontColor} from "../common/utils";
+import "./content.css";
 
 interface Children {
   children: ReactNode;
@@ -17,12 +18,26 @@ export function Content({children}: Children){
     backgroundImage: `linear-gradient(${gradientStart}, white)`
   };
   const fontColor = calculateFontColor({sum, red, green, blue});
-
+  // TODO: 按照季节开、关
   return (
     <main className="woh__site-content" style={style}>
+      <Snowflakes/>
       <SiteTitleString fontColor={fontColor}/>
       <NavBar fontColor={fontColor}/>
       {children}
     </main>
   );
+}
+
+function Snowflakes(){
+    const flakes = Array.from({ length: 10 }, (_, i) => i);
+    return (
+      <div className="woh__snowflakes" aria-hidden="true">
+        {flakes.map((_: number, index: number) => (
+          <div key={index} className="woh__single-snowflake">
+            <div className="inner">❅</div>
+          </div>
+        ))}
+      </div>
+    );
 }
