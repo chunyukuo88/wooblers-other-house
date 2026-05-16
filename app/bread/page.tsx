b2cba107e9ddc2b17c89af6de7869e444c424563
chunyukuo88 <alexgochenour@gmail.com>
2026-05-15 22:42:14 -0400

    chore: pruned bread page

--- app/bread/page.tsx
+++
@@ -1,7 +0,0 @@
-import BreadGallery from "@/components/galleries/bread-gallery_DEPRECATED/bread-gallery";
-import {getBreadImages} from "../../common/http";
-
-export default async function Page(){
-  const breadObjects = await getBreadImages();
-  return <BreadGallery breadObjects={breadObjects}/>;
-}
