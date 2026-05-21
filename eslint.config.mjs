8e7fbec33f7190f2150ed537e5579d42d2f252e4
chunyukuo88 <alexgochenour@gmail.com>
2026-05-21 01:25:56 -0400

    chore: configs

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import next from 'eslint-config-next';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+    ...next,
+    prettierConfig,
+    {
+        ignores: ['.next/**', 'node_modules/**', 'coverage/**'],
+    },
+    {
+        rules: {
+            'no-console': 'warn',
+            'react-hooks/exhaustive-deps': 'error',
+            '@next/next/no-img-element': 'error'
+        }
+    }
+];
