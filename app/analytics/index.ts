b31f88bffd57a7e53132383f4c70a2a6b3a7ca3d
chunyukuo88 <alexgochenour@gmail.com>
2026-05-20 23:34:13 -0400

    refactor: improved tests

--- app/analytics/index.ts
+++ app/analytics/index.ts
@@ -1,5 +1,3 @@
-export type EventName = 'clicked_woobler' | 'adjust-colors';
-
 export const trackEvent = (eventName: string, params?: Record<string, string>) => {
     if (typeof window !== "undefined" && typeof window.gtag !== "undefined") {
         window.gtag("event", eventName, params);
