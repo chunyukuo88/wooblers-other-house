f9b40a15a0885921b6d9bb67a4e1daa4ba77775a
chunyukuo88 <alexgochenour@gmail.com>
2026-07-02 09:05:24 -0400

    fix: album selector
    - It now maintains the proper global
    - state for currently selected folder.
    - User can select a folder, navigate
    - elsewhere, then return to main gallery
    - and still be on their selected folder.

--- components/navigation/components/album-selector.tsx
+++ components/navigation/components/album-selector.tsx
@@ -1,14 +1,14 @@
 'use client';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { useMainImages } from '../../../store';
-import { Folder } from 'store/fetched-images/types';
+import { emptyFolder, Folder } from 'store/fetched-images/types';
 import '../styles/album-selector.css';
 
 export const AlbumSelector = (props: any) => {
   const { style } = props;
-  const { fetchedFolders, updateCurrentFolder } = useMainImages();
+  const { fetchedFolders, currentFolder, updateCurrentFolder } = useMainImages();
   const [folders, setFolders] = useState<Folder[]>([]);
-  const [current, setCurrent] = useState<string>('');
+  const [current, setCurrent] = useState<Folder>(emptyFolder);
 
   const changeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
     const index = event.target.selectedIndex;
@@ -22,24 +22,36 @@ export const AlbumSelector = (props: any) => {
     }
   }, [fetchedFolders]);
 
+  useEffect(() => {
+    if (currentFolder) {
+      setCurrent(currentFolder);
+    }
+  }, [currentFolder]);
+
   return (
     <select name="album-picked" id="woh__album-picker" onChange={changeHandler} style={style}>
-      {!folders?.length ? <AlbumsLoading /> : <Albums folders={folders} />}
+      {!folders?.length ? <AlbumsLoading /> : <Albums folders={folders} current={current} />}
     </select>
   );
 };
 
 type AlbumsProps = {
   folders: Folder[];
+  current: Folder;
 };
 
 function Albums(props: AlbumsProps) {
-  const { folders } = props;
+  const { folders, current } = props;
   return (
     <>
       {folders.map((folder, index) => {
         return (
-          <option className="woh__album-picker__option" key={index} value={folder.friendlyName}>
+          <option
+            className="woh__album-picker__option"
+            key={index}
+            value={folder.friendlyName}
+            selected={folder.name === current.name}
+          >
             {folder.friendlyName || index}
           </option>
         );
