ec690b79bdc8f20c9370754d8177a9d5d29a5bf9
chunyukuo88 <alexgochenour@gmail.com>
2026-07-06 18:58:37 -0400

    feat: convert friendly name to query param
    - Spaces become hyphens

--- app/utils.ts
+++ app/utils.ts
@@ -7,6 +7,14 @@ type GetFoldersResult = {
 };
 
 /**
+ *
+ * */
+export function convertFriendlyToQueryParam(friendlyName: string): string {
+  const spacesToHyphens = friendlyName.split(' ').join('-').toLowerCase();
+  return spacesToHyphens.replace('!', '%21');
+}
+
+/**
  * Contract: The only special char used by album params is exclamation marks.
  * @param searchParam the friendly album name, with URL conversions (such as for special chars)
  * @returns the friendly album name, without URL conversions
