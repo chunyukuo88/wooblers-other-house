7d81d85aabc4ba7acdc99daf52e504902e4048e7
chunyukuo88 <alexgochenour@gmail.com>
2026-07-09 20:44:03 -0400

    feat: first two tests passing.
    - An interesting exercise; learned that
    - the navigator has an async share()
    - method and a clipboard object with
    - an async writeText() method.

--- components/navigation/components/album-selector/__tests__/utils.test.ts
+++ components/navigation/components/album-selector/__tests__/utils.test.ts
@@ -1,11 +1,57 @@
-import {} from '../utils';
+import { handleShare } from '../utils';
 
 describe('handleShare()', () => {
-  describe('GIVEN: the user is viewing private images', () => {
-    describe('WHEN: the user is viewing public images', () => {
-      describe('WHEN: the user clicks the share button', () => {
-        it("THEN: copies the URL to the user's clipboard without modification.", () => {
-          //
+  describe('WHEN: the user is viewing public images', () => {
+    const theUrl = 'https://www.wooblers-other-house.com/?album=onomichi-trip';
+
+    describe('WHEN: user clicks share button,', () => {
+      describe('AND: the device allows use of the Navigator API share() method', () => {
+        it("THEN: copies the URL to the user's clipboard without modification.", async () => {
+          delete global.window.location;
+          global.window = Object.create(window);
+          global.window.location = {
+            href: theUrl,
+          };
+
+          delete global.navigator;
+          global.navigator = {
+            share: jest.fn(),
+          };
+
+          const spy = jest.spyOn(global.navigator, 'share');
+
+          await handleShare();
+
+          expect(spy).toHaveBeenCalledWith({ url: theUrl });
+        });
+      });
+      describe('AND: the device does NOT allow the Navigator API share() method,', () => {
+        it("THEN: copies the URL to the user's clipboard without modification.", async () => {
+          const mockWriteText = jest.fn();
+          delete global.window.location;
+          global.window = Object.create(window);
+          global.window.location = {
+            href: theUrl,
+          };
+
+          delete global.navigator.clipboard;
+          global.navigator = Object.create(navigator);
+          global.navigator.share = undefined;
+          global.navigator.clipboard = {
+            writeText: mockWriteText,
+            read: jest.fn(),
+            readText: jest.fn(),
+            write: jest.fn(),
+            addEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
+            removeEventListener: jest.fn(),
+          };
+
+          const writeTextSpy = jest.spyOn(global.navigator.clipboard, 'writeText');
+
+          await handleShare();
+
+          expect(writeTextSpy).toHaveBeenCalledWith(theUrl);
         });
       });
     });
--- components/navigation/components/album-selector/utils.ts
+++ components/navigation/components/album-selector/utils.ts
@@ -5,6 +5,11 @@ export const updateUrl = (asQueryParams: string): void => {
   window.history.replaceState(null, '', newUrl);
 };
 
-export const handleShare = () => {
-  //
+export const handleShare = async () => {
+  const baseUrl = window.location.href;
+  if (navigator.share) {
+    await navigator.share({ url: baseUrl });
+  } else {
+    await navigator.clipboard.writeText(baseUrl);
+  }
 };
