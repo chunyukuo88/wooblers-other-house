bf0c9477f5c4d8f9477c83ea52599fb5a62730f4
chunyukuo88 <alexgochenour@gmail.com>
2026-07-14 17:46:56 -0400

    chore: removed logging

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import ImageGallery from '@/components/galleries/main-gallery/image-gallery';
 import { getFolders } from './utils';
 import { Metadata } from 'next';
 import { convertAlbumParamToFriendly } from 'store/album/utils';
-import { logger } from '../common/logging';
 
 export const dynamic = 'force-dynamic';
 
@@ -13,13 +12,9 @@ type Params = {
 };
 
 export default async function Page({ searchParams }: Params) {
-  logger('🏠 Homepage() 0');
   const params = await searchParams;
-  logger('🏠 Homepage() 1');
   const privateImageQuery = params[process.env.NEXT_PUBLIC_FF_PRIVATE_IMAGES_KEY!];
-  logger('🏠 Homepage() 2');
   const { displayPrivateImages, folders } = await getFolders(privateImageQuery);
-  logger('🏠 Homepage() 3');
   if (!folders) {
     return <div>The site is down for maintenance. Come back tomorrow!</div>;
   }
--- app/utils.ts
+++ app/utils.ts
@@ -1,6 +1,5 @@
 import { getFeatureStatus } from './flags';
 import { Folder, getMainPageImages } from '../common/http';
-import { logger } from '../common/logging';
 
 type GetFoldersResult = {
   displayPrivateImages: boolean;
@@ -8,13 +7,9 @@ type GetFoldersResult = {
 };
 
 export async function getFolders(searchParam: string): Promise<GetFoldersResult> {
-  logger('getFolders() 0');
   const displayPrivateImages = await getFeatureStatus(searchParam);
-  logger('getFolders() 1 - displayPrivateImages', displayPrivateImages);
   const unprocessedFolders = await getMainPageImages(displayPrivateImages);
-  logger('getFolders() 2 - unprocessedFolders', unprocessedFolders);
   const sansThumbnails = removeThumbnails(unprocessedFolders);
-  logger('getFolders() 3 - sansThumbnails', sansThumbnails);
 
   return {
     displayPrivateImages,
@@ -23,13 +18,10 @@ export async function getFolders(searchParam: string): Promise<GetFoldersResult>
 }
 
 const removeThumbnails = (folders: Folder[]) => {
-  logger('🍎 removeThumbnails 0');
   if (!folders) {
     return [];
   }
-  logger('🍎 removeThumbnails 1');
   return folders.map((folder: Folder, i: number) => {
-    logger('🍎 removeThumbnails 2, index: ', i);
     const photosSansThumbnail = folder.photos.filter((photo: string) => {
       const isThumbnail = photo.split('.').find((part) => part.includes('thumbnail'));
       return !isThumbnail;
