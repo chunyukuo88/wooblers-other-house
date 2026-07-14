e1cd2a15f406a9d25cd51410081dc1f95ca3b82b
chunyukuo88 <alexgochenour@gmail.com>
2026-07-13 20:33:24 -0400

    feat: guard clauses

--- app/flags.ts
+++ app/flags.ts
@@ -5,21 +5,33 @@ const activationValue = process.env.NEXT_PUBLIC_FF_PRIVATE_IMAGES_ON!;
 
 function enabledByFeatureFlag(flagValue: string) {
   logger('enabled byFeatureFlag 0 - flagValue', flagValue);
+  if (!flagValue) {
+    return false;
+  }
   logger('enabled byFeatureFlag 1 - activationValue', activationValue);
   return flagValue === activationValue;
 }
 
 async function enabledByCookie(queryParamKey: string) {
   logger('enabledByCookie 0 - queryParamKey', queryParamKey);
+  if (!queryParamKey) {
+    return false;
+  }
   const cookieStore = await cookies();
   logger('enabledByCookie 1');
   const activationCookieValue = cookieStore.get(queryParamKey)?.value;
+  if (!activationCookieValue) {
+    return false;
+  }
   logger('enabledByCookie 2 - activationCookieValue', activationCookieValue);
   return activationCookieValue === activationValue;
 }
 
 export async function getFeatureStatus(queryParamKey: string) {
   logger('getFeatureStatus() 0');
+  if (!queryParamKey) {
+    return false;
+  }
   const featureEnabledByQueryParams = enabledByFeatureFlag(queryParamKey);
   logger('getFeatureStatus() 1');
   const featureEnabledByCookie = enabledByCookie(queryParamKey);
