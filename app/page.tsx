221762511d4d595d35a16970821b3f4152bb7c30
chunyukuo88 <alexgochenour@gmail.com>
2026-07-25 13:20:41 -0400

    feat: hardened against Lambda failure

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { ImageGallery } from '@/components/galleries/main-gallery';
 import { getFolders } from './utils';
 import { Metadata } from 'next';
 import { convertAlbumParamToFriendly } from 'store/album/utils';
+import './page.css';
 
 export const dynamic = 'force-dynamic';
 
@@ -15,8 +16,15 @@ export default async function Page({ searchParams }: Params) {
   const params = await searchParams;
   const privateImageQuery = params[process.env.NEXT_PUBLIC_FF_PRIVATE_IMAGES_KEY!];
   const { displayPrivateImages, folders } = await getFolders(privateImageQuery);
-  if (!folders) {
-    return <div>The site is down for maintenance. Come back tomorrow!</div>;
+
+  if (!folders.length) {
+    return (
+      <div id="woh__site-down-for-maintenance">
+        The site is down for maintenance 🍵
+        <br />
+        Come back tomorrow!
+      </div>
+    );
   }
 
   const albumQueryParameter = params['album'];
